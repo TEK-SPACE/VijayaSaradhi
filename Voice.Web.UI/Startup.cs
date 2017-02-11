5bf5d0a7282ada4df872b2e113ae1b90ba9ca2c8
Harsha Bopuri <hbopuri@hotmail.com>
2017-02-11 19:23:31 +0530

    Main Voice Study

--- Voice.Web.UI/Startup.cs
+++ Voice.Web.UI/Startup.cs
@@ -1,14 +1,35 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-[assembly: OwinStartupAttribute(typeof(Voice_Maate.Startup))]
-namespace Voice_Maate
+namespace Voice.Web.UI
 {
-    public partial class Startup
+    public class Startup
     {
-        public void Configuration(IAppBuilder app)
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
         {
-            ConfigureAuth(app);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
